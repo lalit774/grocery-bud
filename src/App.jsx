8e086664ed217324b5be6925b3ee262fa3f59163
lalit kumar <lalit.kumar@socialinnovation.ca>
2023-10-15 16:36:14 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
-const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
-};
+import { useState } from 'react'
+import { Form } from './Form'
+import { nanoid } from 'nanoid'
+import Item from './Item'
+import { ToastContainer, toast } from 'react-toastify'
 
-export default App;
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items))
+}
+
+const defaultList = JSON.parse(localStorage.getItem('list') || '[]')
+
+const App = () => {
+  const [items, setItems] = useState(defaultList)
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    }
+    const newItems = [...items, newItem]
+    setItems(newItems)
+    setLocalStorage(newItems)
+    toast.success('item added to the list')
+  }
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId)
+    setItems(newItems)
+    setLocalStorage(newItems)
+    toast.success('item removed')
+  }
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed }
+        return newItem
+      }
+      return item
+    })
+    setItems(newItems)
+    setLocalStorage(newItems)
+  }
+
+  return (
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form addItem={addItem} />
+      <Item items={items} removeItem={removeItem} editItem={editItem} />
+    </section>
+  )
+}
+
+export default App
